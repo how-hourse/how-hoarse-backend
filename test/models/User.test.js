793acd4eaddca35de66ac75bde39061a92b30326
UncleJerry <jared.myhrberg@gmail.com>
2019-06-05 11:37:17 -0700

    User test passing, create user

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
 const User = require('../../lib/models/User');
 
 describe('user', () => {
-  
+  beforeAll(() => {
+    return connect();
+  });
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
   it('hashes the password at user creation', () => {
     return User.create({
       name: 'beef taco 69',
@@ -12,7 +23,7 @@ describe('user', () => {
       .then(user => {
         expect(user.toJSON()).toEqual({
           name: 'beef taco 69',
-          email: '1234567890',
+          email: 'person@gmail.com',
           _id: expect.any(mongoose.Types.ObjectId),
         });
       });
